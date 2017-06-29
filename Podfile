9278ba9aab91698df32b64fb3d8dfec53bb9d9af
alexleow <leowfg@gmail.com>
2017-06-29 17:01:23 +0800

    add sdk library

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SocialWallet-iOS-SDK-Demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SocialWallet-iOS-SDK-Demo
+  pod 'SocialWalletSDK'
+
+  target 'SocialWallet-iOS-SDK-DemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SocialWallet-iOS-SDK-DemoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- SocialWallet-iOS-SDK-Demo/AppDelegate.swift
+++ SocialWallet-iOS-SDK-Demo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SocialWalletSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -35,6 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+            SocialWalletSDK.logToConsole(msg: "Application active")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
