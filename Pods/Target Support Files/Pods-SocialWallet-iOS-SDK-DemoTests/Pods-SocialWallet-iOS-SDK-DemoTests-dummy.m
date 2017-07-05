029146be731fba998895ff1e9addb481e2537d93
alexleow <leowfg@gmail.com>
2017-07-05 17:41:01 +0800

    1st sdk

--- Pods/Target Support Files/Pods-SocialWallet-iOS-SDK-DemoTests/Pods-SocialWallet-iOS-SDK-DemoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SocialWallet_iOS_SDK_DemoTests : NSObject
-@end
-@implementation PodsDummy_Pods_SocialWallet_iOS_SDK_DemoTests
-@end
--- Pods/Target Support Files/Pods-SocialWallet-iOS-SDK-DemoUITests/Pods-SocialWallet-iOS-SDK-DemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SocialWallet_iOS_SDK_DemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SocialWallet_iOS_SDK_DemoUITests
-@end
